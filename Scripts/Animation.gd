70d48a9a2b8b6c31515d3283a4cc05f74d8b0166
wetrain <filipgamer987@gmail.com>
2025-04-19 14:43:49 +0200

    Lights

--- Scripts/Animation.gd
+++ Scripts/Animation.gd
@@ -3,7 +3,7 @@ extends AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$/root/Node3D/Houses/house52/Flicker.play("Flicker")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
