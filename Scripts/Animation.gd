5ee946317452bb2aae2fb1a918c55637dcae442e
wetrain <filipgamer987@gmail.com>
2025-04-27 20:44:52 +0200

    Remove

--- Scripts/Animation.gd
+++ Scripts/Animation.gd
@@ -3,7 +3,7 @@ extends AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$/root/Node3D/Houses/house12/house1.play("Flicker")
+	$/root/Node3D/Houses/house12/house1/Flicker.play("Flicker")
 	$/root/Node3D/Houses/house12/house1/house1_door1/Sway.play("Sway")
 
 
@@ -11,7 +11,3 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if $/root/Node3D/Sitting/AnimationPlayer.is_playing() == false and Globals.potatoSwing == true:
 		$/root/Node3D/Sitting/AnimationPlayer.play("mixamo_com")
-	if $/root/Node3D/Houses/house22/house2/Hinge/house2_door1/Flicker.is_playing() == false:
-		$/root/Node3D/Houses/house22/house2/Hinge/house2_door1/Flicker.play("Flicker")
-	if $/root/Node3D/Monster/AnimationPlayer.is_playing() == false:
-		$/root/Node3D/Monster/AnimationPlayer.play("zoonamaly_monster2_Monster2_skeleton|Jumpscare")
