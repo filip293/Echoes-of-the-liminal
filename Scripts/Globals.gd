8f7d42911a505ef73793523e8a291dafdf59950b
squaapi <squaapi@gmail.com>
2025-07-13 17:51:24 +0200

    Sequence Correction script created.

--- Scripts/Globals.gd
+++ Scripts/Globals.gd
@@ -7,6 +7,12 @@ var beginningcutsceneended: bool = false
 var entered_village = false
 var mouse_sensitivity = 0.2
 
+var scenes = {
+	"S1" : false,
+	"S2" : false,
+	"S3" : false
+}
+
 var potatoSwing = true
 
 func calltime(time) -> void:
