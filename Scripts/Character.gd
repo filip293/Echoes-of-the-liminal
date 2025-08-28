bfd7c37cae54819a4323d3df3ac8f0a5ce9666c6
squaapi <squaapi@gmail.com>
2025-08-28 06:13:03 +0200

    Velocity-based headbob and FOV adjustments. Placeholder stamina added. Deleted old code that we aren't using anymore.

--- Scripts/Character.gd
+++ Scripts/Character.gd
@@ -9,6 +9,24 @@ extends CharacterBody3D
 @export var clamp_up_deg: float = 70.0
 @export var clamp_down_deg: float = -90.0
 
+@export_group("Movement")
+@export var walk_speed: float = 3.0
+@export var sprint_speed: float = 6.0
+
+@export_group("Headbob")
+@export var bob_frequency: float = 2.0
+@export var bob_amplitude: float = 0.06
+@export var headbob_return_speed: float = 8.0
+
+@export_group("FOV")
+@export var base_fov: float = 75.0
+@export var fov_change: float = 1.5
+
+@export_group("Stamina")
+@export var max_stamina: float = 100.0
+@export var stamina_drain_rate: float = 15.0
+@export var stamina_regen_rate: float = 20.0
+@export var stamina_regen_delay: float = 1.0
 
 @onready var neck := $Neck
 @onready var camera := $Neck/Camera
@@ -16,8 +34,10 @@ extends CharacterBody3D
 @onready var right_foot_audio := $RightFootAudio
 @onready var mnst_lf_audio := $MonsterSteps/LeftFootAudio
 @onready var mnst_rf_audio := $MonsterSteps/RightFootAudio
-const SPEED = 2
-const SPRINT_MULTIPLIER = 1.65 * 7
+@onready var stamina_bar = $/root/Node3D/InstViewport/Stamina/StaminaBar
+
+var current_stamina: float
+var stamina_regen_timer: float = 0.0
 var footstep_timer = 0.0
 var sec_footstep_timer = 0.0
 var is_left_foot = false
@@ -28,12 +48,13 @@ var walking = true
 
 var internaloverride = false
 var village_entered = false
-const FOOTSTEP_INTERVAL = 1.8 / SPEED
+var t_bob: float = 0.0
 
 var mouse_velocity := Vector2.ZERO
 var camera_rotation_deg := Vector2.ZERO
 var actual_velocity := Vector2.ZERO
 var target_velocity := Vector2.ZERO
+var camera_original_pos: Vector3
 
 var dirt_footstep_sounds = [
 	preload("res://Sounds/Steps_dirt-001.ogg"),
@@ -62,7 +83,11 @@ func release_control():
 func _ready():
 	$/root/Node3D/Houses/house12/house1/Flicker.play("Flicker")
 	$/root/Node3D/Houses/house42/house4/house1_door1/Sway.play("Sway")
-
+	camera_original_pos = camera.position
+	current_stamina = max_stamina
+	if stamina_bar:
+		stamina_bar.visible = false
+	
 	await Globals.gamestart
 	camera_rotation_deg.y = rad_to_deg(self.rotation.y)
 	camera_rotation_deg.x = rad_to_deg(neck.rotation.x)
@@ -75,10 +100,7 @@ func _ready():
 	DialogueManager.show_dialogue_balloon(load("res://Dialogue/Shadow.dialogue"), "Shadow")
 	await Globals.calltime(1.2)
 	$Animations.play("Look_Up")
-	await Globals.calltime(5)
-	#DialogueManager.show_dialogue_balloon(load("res://Dialogue/dialogue.dialogue"), "Potato")
-	
-	await Globals.calltime(1)
+	await Globals.calltime(6)
 	$/root/Node3D/Shadow/AnimationPlayer.play("Sitting")
 	await DialogueManager.dialogue_ended
 	
@@ -120,77 +142,92 @@ func _process(delta: float) -> void:
 		
 	if monsterfollowing and (velocity.x != 0.0 or velocity.z != 0.0):
 		sec_footstep_timer += delta
-		if sec_footstep_timer >= FOOTSTEP_INTERVAL + 0.78:
+		var footstep_inter = 1.8 / walk_speed
+		if sec_footstep_timer >= footstep_inter + 0.78:
 			sec_footstep_timer = 0
 			play_monster_following_footsteps()
 		
 func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity.y -= delta * ProjectSettings.get_setting("physics/3d/default_gravity")
+
 	var input_dir := Input.get_vector("Left", "Right", "Forwards", "Back")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-
-	velocity.y -= delta * ProjectSettings.get_setting("physics/3d/default_gravity")
+	var target_camera_pos: Vector3
+	
+	var can_sprint = Input.is_action_pressed("Sprint") and is_on_floor() and direction != Vector3.ZERO and current_stamina > 0
+	
+	var speed = walk_speed
+	if can_sprint and Globals.playermoveallow:
+		speed = sprint_speed
+		current_stamina = max(0, current_stamina - stamina_drain_rate * delta)
+		stamina_regen_timer = 0
+	else:
+		if not Input.is_action_pressed("Sprint"):
+			stamina_regen_timer += delta
+			if stamina_regen_timer >= stamina_regen_delay:
+				current_stamina = min(max_stamina, current_stamina + stamina_regen_rate * delta)
 
 	if direction and Globals.playermoveallow:
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
 		walking = true
-		var current_speed = SPEED
-		if Input.is_action_pressed("Sprint"):
-			current_speed *= SPRINT_MULTIPLIER
-		velocity.x = direction.x * current_speed
-		velocity.z = direction.z * current_speed
-
+		
+		var footstep_inter = (1.8 / walk_speed) * (walk_speed / speed)
 		footstep_timer += delta
-		var footstep_inter = FOOTSTEP_INTERVAL * (SPEED / current_speed)
 		if footstep_timer >= footstep_inter:
 			footstep_timer = 0
 			play_footstep_sound()
+			
+		t_bob += delta * velocity.length() * float(is_on_floor())
+		target_camera_pos = camera_original_pos + _headbob(t_bob)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
 		walking = false
+		target_camera_pos = camera_original_pos
+
+	camera.position = lerp(camera.position, target_camera_pos, delta * headbob_return_speed)
+	
+	var velocity_clamped = clamp(velocity.length(), 0.5, sprint_speed * 2)
+	var target_fov = base_fov + fov_change * velocity_clamped
+	camera.fov = lerp(camera.fov, target_fov, delta * 8.0)
+
+	if stamina_bar:
+		stamina_bar.max_value = max_stamina
+		stamina_bar.value = current_stamina
+
+	if Globals.playermoveallow and can_sprint or current_stamina < max_stamina:
+		stamina_bar.visible = true
+	else:
+		stamina_bar.visible = false
 
 	move_and_slide()
 
+func _headbob(time: float) -> Vector3:
+	var pos = Vector3.ZERO
+	pos.y = sin(time * bob_frequency) * bob_amplitude
+	pos.x = cos(time * bob_frequency / 2) * bob_amplitude
+	return pos
+
 func play_monster_following_footsteps():
 	if is_left_foot:
-		mnst_lf_audio.stream = footstep_sounds[randi() % 3] # Randomly select a sound
+		mnst_lf_audio.stream = footstep_sounds[randi() % 3]
 		mnst_lf_audio.play()
 	else:
-		mnst_rf_audio.stream = footstep_sounds[randi() % 3] # Randomly select a sound
+		mnst_rf_audio.stream = footstep_sounds[randi() % 3]
 		mnst_rf_audio.play()
 		
 func play_footstep_sound():
 	if Globals.playermoveallow:
 		if is_left_foot:
-			left_foot_audio.stream = footstep_sounds[randi() % 3] # Randomly select a sound
+			left_foot_audio.stream = footstep_sounds[randi() % 3]
 			left_foot_audio.play()
 		else:
-			right_foot_audio.stream = footstep_sounds[randi() % 3] # Randomly select a sound
+			right_foot_audio.stream = footstep_sounds[randi() % 3]
 			right_foot_audio.play()
 
-		is_left_foot = !is_left_foot # Alternate foot
-
-func _on_look_behind_screen_entered() -> void:
-	#if $"../Sitting/LookBehind" != null:
-		#Globals.cameramoveallow = false
-		#$"../Sitting/LookBehind".queue_free()
-		#$"../Sitting/Skeleton3D".queue_free()
-		#Globals.potatoSwing = false
-		#$/root/Node3D/Sitting/AnimationPlayer.stop()
-	await Globals.calltime(1)
-	DialogueManager.show_dialogue_balloon(load("res://Dialogue/dialogue.dialogue"), "Look_Behind")
-	await DialogueManager.dialogue_ended
-	Globals.cameramoveallow = true
-	Look_Behind = true
-	
-
-#func _on_look_at_potato_screen_entered() -> void:
-	#if Look_Behind == true:
-		#$"../Sitting/LookAtPotato".queue_free()
-		#await Globals.calltime(1)
-		#DialogueManager.show_dialogue_balloon(load("res://Dialogue/dialogue.dialogue"), "Potato_gone")
-		#Globals.playermoveallow = true
-		#await Globals.calltime(8)
-		#Globals.beginningcutsceneended = true
+		is_left_foot = !is_left_foot
 		
 func _on_static_body_3d_body_entered(body: Node) -> void:
 	if body is CharacterBody3D and body.name == "CharacterBody3D" and $TempBranchBreak != null:
